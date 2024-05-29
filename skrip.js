bcd5c2dd10c374c36bbaa98526d4e20ee507c601
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-05-29 21:08:29 +0500

    gfyf

---
+++ skrip.js
@@ -0,0 +1,7 @@
+let a =prompt ("ismingizni kritingi");
+let b =+prompt("yosh kriting");
+let d =prompt("malumot kiriting");
+let c =confirm("tasdiqlash");
+alert ( typeof a ) ;
+alert( typeof b);
+alert( typeof d);
